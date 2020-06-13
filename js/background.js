d1a6ac8580b57b5b78d04276c4da2f7e91bcc308
Carlos Eustáquio <caeu227@gmail.com>
2020-06-13 13:18:29 -0300

    prettier + add list event after alarm

--- js/background.js
+++ js/background.js
@@ -9,19 +9,26 @@ function deleteAlarm(allAlarms, currentAlarm) {
 chrome.alarms.onAlarm.addListener(function (alarm) {
   const alarmId = parseInt(alarm.name.split(".")[1]);
 
-  chrome.storage.sync.get("alarms", function ({ alarms }) {
+  chrome.storage.sync.get("alarms", async function ({ alarms }) {
     const currentAlarm = alarms.find((al) => al.id === alarmId);
     console.log(currentAlarm);
 
-    chrome.notifications.create(`${currentAlarm.id}`, {
-      type: "basic",
-      iconUrl: "timebox_logo128.png",
-      title: `TimeBox end!`,
-      message: `${currentAlarm.label}`,
-      buttons: [{ title: "Thanks!" }],
-      priority: 0,
-    });
-
-    deleteAlarm(alarms, currentAlarm);
+    chrome.notifications.create(
+      `${currentAlarm.id}`,
+      {
+        type: "basic",
+        iconUrl: "timebox_logo128.png",
+        title: `TimeBox end!`,
+        message: `${currentAlarm.label}`,
+        buttons: [{ title: "Thanks!" }],
+        priority: 0,
+      },
+      function () {
+        deleteAlarm(alarms, currentAlarm);
+        chrome.runtime.sendMessage({
+          msg: "list_timeboxes",
+        });
+      }
+    );
   });
 });
